614d43aaae50ff067e836fba9c35acfbfca65ba0
fabricekx <fabricekx@gmail.com>
2024-03-28 13:10:16 +0100

    Ajout du gestionnaire d'assets Asset_Mapper, création du registrationFormType et du UserController

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('birthday', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('status')
+            ->add('pseudo')
+            ->add('avatar')
+            ->add('date', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

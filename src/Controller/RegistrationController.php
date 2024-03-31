3c6b595aa6b6d3b9ba1e1a2ab6c76388c731de35
fabricekx <fabricekx@gmail.com>
2024-03-31 17:51:56 +0200

    gestion des fichiers images lors ed l'enregistrement et affichage des avatars dans profile. Attention, il faut encore refaire le meme code dans UserController, voir à faire un service dédié).

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,6 +8,7 @@ use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mime\Address;
@@ -37,7 +38,28 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-            
+
+            // gestion du fichier image
+            $avatarFile = $form->get('avatar')->getData();
+
+            // Vérifier si un fichier a été téléchargé
+            if ($avatarFile) {
+                // Générer un nom de fichier unique, uniquid permet de créer un nom à partir de la date et l'heure, pour avoir un nom unique
+                $newFilename = uniqid().'.'.$avatarFile->guessExtension();
+
+                // Déplacer le fichier vers le répertoire où vous souhaitez le stocker
+                try {
+                    $avatarFile->move(
+                        $this->getParameter('kernel.project_dir').'/public/images/avatars', // Répertoire de destination
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Gérer l'erreur si le déplacement du fichier échoue
+                }
+
+                // Enregistrez le nom de fichier de l'image dans l'entité User
+                $user->setAvatar($newFilename);
+            }
             $this->addFlash(
                 'success',
                 'Votre compte a été créé, vous avez reçu un mail de confirmation'
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,7 +30,7 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
-         
+
             if ($form->isValid()) {
                 $entityManager->persist($this->getUser()); // insérer en base
                 $entityManager->flush(); // fermer la transaction executée par la bdd
@@ -39,6 +39,10 @@ class UserController extends AbstractController
                     'success',
                     'Votre profile a bien été mis à jour !'
                 );
+                // récupération de l'image de orofil
+                // $user = $this->getUser();
+                // $avatarFilename = $user->getAvatar();
+                // $avatarPath = '/public/images/avatars' . $avatarFilename; // Remplacez ceci par le chemin réel de votre répertoire d'avatars
 
                 return $this->redirectToRoute('app_user', [], Response::HTTP_SEE_OTHER);
 
@@ -56,37 +60,37 @@ class UserController extends AbstractController
     #[Route('/profile/edit_pwd', name: 'app_profile_edit_pwd')]
     public function modify_pwd(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordEncoder): Response
     {
-if($request->isMethod('POST')) {
-$user = $this->getUser();
+        if ($request->isMethod('POST')) {
+            $user = $this->getUser();
 
-// on vérifie que les deux mots de passe sont identiques
-if ($request->request->get('password1') == $request->request->get('password2')) {
-     // Vérifier si le mot de passe respecte les contraintes
-     $password1 = $request->request->get('password1');
-     if (strlen($password1) < 6 || !preg_match('/[0-9]/', $password1) || !preg_match('/[!@#$%^&*-]/', $password1)) {
-        $this->addFlash('errors', 'Votre mot de passe doit contenir au moins 6 caractères et un caractère spécial');
-        return $this->redirectToRoute('app_profile_edit_pwd');
-    }
-    
-    
-    // on hash le nouveau mot de passe et on le modifie 
-    $newEncodedPassword = $passwordEncoder->hashPassword($user, $request->request->get('password1'));
-$user->setPassword( $newEncodedPassword);
-$entityManager->persist($this->getUser()); // insérer en base
-$entityManager->flush(); // fermer la transaction executée par la bdd
-$this->addFlash('success', 'Votre mot de passe a été modifié');
-return $this->redirectToRoute('app_user');
-} else {
-    $this->addFlash('errors' , 'Les mots de passe ne sont pas identiques');
-
-}
+            // on vérifie que les deux mots de passe sont identiques
+            if ($request->request->get('password1') == $request->request->get('password2')) {
+                // Vérifier si le mot de passe respecte les contraintes
+                $password1 = $request->request->get('password1');
+                if (strlen($password1) < 6 || !preg_match('/[0-9]/', $password1) || !preg_match('/[!@#$%^&*-]/', $password1)) {
+                    $this->addFlash('errors', 'Votre mot de passe doit contenir au moins 6 caractères et un caractère spécial');
+                    return $this->redirectToRoute('app_profile_edit_pwd');
+                }
 
 
-}
+                // on hash le nouveau mot de passe et on le modifie 
+                $newEncodedPassword = $passwordEncoder->hashPassword($user, $request->request->get('password1'));
+                $user->setPassword($newEncodedPassword);
+                $entityManager->persist($this->getUser()); // insérer en base
+                $entityManager->flush(); // fermer la transaction executée par la bdd
+                $this->addFlash('success', 'Votre mot de passe a été modifié');
+                return $this->redirectToRoute('app_user');
+            } else {
+                $this->addFlash('errors', 'Les mots de passe ne sont pas identiques');
+
+            }
+
+
+        }
 
 
         return $this->render('user/modify_password.html.twig', [
-            
+
         ]);
     }
 }
